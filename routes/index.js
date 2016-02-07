d376cad7a7364e50003869fa0978c77608d8e032
Bloodham <bloodham25@gmail.com>
2016-02-07 02:17:20 -0500

    Test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET contact page. */
